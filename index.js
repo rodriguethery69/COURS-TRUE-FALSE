e1d1c9bc7254e1e67e1f644780cc4a28f503f2da
rodriguethery69 <rodriguethery@gmail.com>
2023-12-05 18:46:48 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log(true && false) // false
+console.log(true && true) // true
+console.log(false && false) // false
+console.log(true || false) //true
+console.log(true || true) //true
+console.log(false || false) //false
